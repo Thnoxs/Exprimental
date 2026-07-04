7abc3a2b4080fbf8cb400fd3636a154aef6dd8b2
Thnoxs <thnoxs@gmail.com>
2026-07-04 09:54:18 +0530

    Added: Image preview

--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
-// Select all the links within your tab list
 const tabs = document.querySelectorAll(".project-tab-list li");
-const TabtextColor = document.querySelectorAll(".project-tab-list li a");
+const box = document.querySelector(".project-box");
 const click = new Audio("/assets/Click_main-01.wav");
-tabs.forEach((tab) => {
-  tab.addEventListener("click", function (event) {
-    click.play();
-    // 1. Remove .active class from all tabs
-    tabs.forEach((item) => item.classList.remove("active"));
 
-    // 2. Add .active class to the clicked tab
-    this.classList.add("active");
+const imageUrls = [
+  "url(/assets/Gallery/image-1.jpg)",
+  "url(/assets/Gallery/image-2.jpg)",
+  "url(/assets/Gallery/image-3.jpg)",
+  "url(/assets/Gallery/image-4.jpg)",
+];
+
+// Function to handle the tab change
+function setActiveTab(index) {
+  // 1. Remove .active class from all tabs
+  tabs.forEach((tab) => tab.classList.remove("active"));
+
+  // 2. Add .active to the clicked tab
+  tabs[index].classList.add("active");
+
+  // 3. Update the background image
+  box.style.background = imageUrls[index];
+  box.style.backgroundSize = "cover"; // Ensures image fits well
+}
+
+// Attach event listeners
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", function () {
+    click.play();
+    setActiveTab(index);
   });
 });
+
+// Set default state on page load (Project 1)
+setActiveTab(0);
